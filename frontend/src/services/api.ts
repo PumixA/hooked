06be5ee6f92a3c224302d86a59a69eaf6343e803
PumixA <melvin.delorme12@gmail.com>
2026-01-22 18:46:13 +0100

    feat(arch): Init React app with Tailwind, Axios & AuthContext (HOOK-35)

---
+++ frontend/src/services/api.ts
@@ -0,0 +1,26 @@
+import axios, { InternalAxiosRequestConfig } from 'axios';
+
+const API_URL = 'http://localhost:3000';
+
+const api = axios.create({
+    baseURL: API_URL,
+    headers: {
+        'Content-Type': 'application/json',
+    },
+});
+
+// Typage strict pour TypeScript
+api.interceptors.request.use(
+    (config: InternalAxiosRequestConfig) => {
+        const token = localStorage.getItem('token');
+        if (token) {
+            config.headers.set('Authorization', `Bearer ${token}`);
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+export default api;
